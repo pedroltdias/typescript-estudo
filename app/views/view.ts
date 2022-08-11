f60ee96cfe42f9d54b1306b03d58af27556abf21
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-08-11 15:43:22 -0300

    encapsulamento das views

--- app/views/view.ts
+++ app/views/view.ts
@@ -11,5 +11,5 @@ export abstract class View<T> {
         this.element.innerHTML = template;
     }
 
-    abstract template(model: T): string;
+    protected abstract template(model: T): string;
 }
