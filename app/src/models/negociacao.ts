2aeaf5ae6b95fc0743c63f4c2abeb5d2e1f4b24e
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-08-15 14:28:04 -0300

    implementacao da interface imprimivel e do service 'imprimir'

--- app/src/models/negociacao.ts
+++ app/src/models/negociacao.ts
@@ -1,9 +1,19 @@
-export class Negociacao {
+import { Imprimivel } from "../utils/imprimivel.js";
+
+export class Negociacao implements Imprimivel{
     constructor(
         private _data: Date, 
         public readonly quantidade: number, 
         public readonly valor: number
     ) {}
+
+    public static criaDe(dataString: string, quantidadeString: string, valorString: string): Negociacao {
+        const exp = /-/g;
+        const date = new Date(dataString.replace(exp, ','));
+        const quantidade = parseInt(quantidadeString);
+        const valor = parseFloat(valorString);
+        return new Negociacao(date, quantidade, valor);
+    }
 
     get volume(): number {
         return this.quantidade * this.valor;
@@ -14,11 +24,11 @@ export class Negociacao {
         return data;
     }
 
-    public static criaDe(dataString: string, quantidadeString: string, valorString: string): Negociacao {
-        const exp = /-/g;
-        const date = new Date(dataString.replace(exp, ','));
-        const quantidade = parseInt(quantidadeString);
-        const valor = parseFloat(valorString);
-        return new Negociacao(date, quantidade, valor);
+    public paraTexto(): string {
+        return `
+            Data: ${this.data},
+            Quantidade: ${this.quantidade},
+            Valor: ${this.valor}
+        `;
     }
 }
