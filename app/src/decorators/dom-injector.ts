7d021584b7db05671af4f9d6ea41ff2963cbc225
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-08-15 11:20:12 -0300

    ajustes finais no decorator dom-injector

--- app/src/decorators/dom-injector.ts
+++ app/src/decorators/dom-injector.ts
@@ -3,9 +3,16 @@ export function domInjector(seletor:string) {
         target:any,
         propertyKey: string
     ) {
+        console.log(`Modificando prototype ${target.constructor.name} e adicionando getter para a propriedade ${propertyKey}`);
+
+        let elemento: HTMLElement;
         const getter = function() {
-            const elemento = document.querySelector(seletor);
-            console.log(`buscando elemento do DOM com o seletor ${seletor} para injetar em ${propertyKey}`)
+            
+            if (!elemento) {
+                elemento = <HTMLElement>document.querySelector(seletor);
+                console.log(`buscando elemento do DOM com o seletor ${seletor} para injetar em ${propertyKey}`);
+            }
+            
             return elemento;
         }
 
