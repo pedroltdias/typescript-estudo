b8331c045ab5526a7e29a22b8816f8e4253da932
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-08-15 10:07:44 -0300

    criacao do decorator inspect

--- app/src/decorators/inspect.ts
+++ app/src/decorators/inspect.ts
@@ -6,7 +6,10 @@ export function inspect() {
     ) {
         const metodoOriginal = descriptor.value;
         descriptor.value = function(...args: any[]) {
+            console.log(`--- Método ${propertyKey}`);
+            console.log(`------ parâmetros: ${JSON.stringify(args)}`);
             const retorno = metodoOriginal.apply(this, args);
+            console.log(`------ retorno: ${JSON.stringify(retorno)}`);
             return retorno;
         }
 
